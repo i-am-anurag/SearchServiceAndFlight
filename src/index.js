c17aa8f27e8f5e96223e4cd61c7be192f2ab309a
Anurag Pandey <anuragpandey8469@gmail.com>
2022-12-16 11:55:51 +0530

    Setup body-parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+const app = require('body-parser');
+
 const {PORT} = require('./config/serverConfig');
 const setupAndCreateServer = async()=> 
 {
     const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({exteded: true}));
+
     app.listen(PORT,()=>{
         console.log(`listening on port${PORT}`);
     });
