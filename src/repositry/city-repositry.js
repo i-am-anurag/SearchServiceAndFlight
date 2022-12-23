fc7970afa31034587e456011f2eb3629f61057b3
Anurag Pandey <anuragpandey8469@gmail.com>
2022-12-23 11:41:22 +0530

    Complete City Repostitry

--- src/repositry/city-repositry.js
+++ src/repositry/city-repositry.js
@@ -1,31 +1,65 @@
 const {City} = require('../models/index');
 
-class CityRepository{
-    async createCity({name, city}) 
+class CityRepository
+{
+    async createCity({name}) 
     {
         try
         {
-            const city = await City.create({name})
+            const city = await City.create({
+                    name
+                });
+
             return city;
         }
         catch(err)
         {
-            throw new Error(err);
+            console.log("Something went wrong when creating city");
+            throw(err);
         }
     }
 
-    async deleteCity({cityId}) 
+    async deleteCity(cityId) 
     {
-        try
+        try 
         {
             await City.destroy({
-                where: {id: cityId},
+                where:{
+                    id: cityId
+                }
             });
+        } catch (error) {
+            console.log("Something went wrong when creating city");
+            throw(error);
         }
-        catch(err)
-        {
-            throw new Error(err);
+    }
+
+    async updateCity(cityId,data)
+    {
+        try {
+            const city = await City.update(data,{
+                where:{
+                    id: cityId
+                }
+            });
+            
+            return city;
+        } catch (error) {
+            console.log("Something went wrong when updating city");
+            throw(error);
         }
+    }
+
+    async getCity(cityId)
+    {
+        try {
+            const city = await City.findByPk(cityId);
+
+            return city;
+        } catch (error) {
+            
+        }
+        
     }
 }
 
